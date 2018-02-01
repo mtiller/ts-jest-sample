9683c95467c7fd460852534346a87238e82fb02f
Michael Tiller <michael.tiller@gmail.com>
2018-02-01 13:25:07 -0500

    This adds a function that creates a stack overflow
    I'm not sure why.  It seems that the issue is related specifically to
    the line:
    
    ```
    evaluatedArgs.push(yield* evaluate(result.body.arguments[ii], result.input, result.environment));
    ```
    
    Because if you comment that out, you don't get the stack overflow.

--- src/core/functions.ts
+++ src/core/functions.ts
@@ -28,3 +28,28 @@ export function evaluate(expr: Expression): number {
         }
     }
 }
+
+/**
+ * Apply procedure or function
+ * @param {Object} proc - Procedure
+ * @param {Array} args - Arguments
+ * @param {Object} self - Self
+ * @returns {*} Result of procedure
+ */
+export function* apply(proc, args, self) {
+    var result;
+    result = yield* applyInner(proc, args, self);
+    while (isLambda(result) && result.thunk === true) {
+        //     // trampoline loop - this gets invoked as a result of tail-call optimization
+        //     // the function returned a tail-call thunk
+        //     // unpack it, evaluate its arguments, and apply the tail call
+        var next = yield* evaluate(result.body.procedure, result.input, result.environment);
+        var evaluatedArgs = [];
+        for (var ii = 0; ii < result.body.arguments.length; ii++) {
+            // COMMENT THIS OUT AND THE STACK ISSUE GOES AWAY...
+            evaluatedArgs.push(yield* evaluate(result.body.arguments[ii], result.input, result.environment));
+        }
+        result = yield* applyInner(next, evaluatedArgs, self);
+    }
+    return result;
+}
