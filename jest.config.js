73eaf1e8f00e3e9e65d345d3478d90372b36ad73
Michael Tiller <michael.tiller@gmail.com>
2018-02-01 11:38:44 -0500

    Ignore generated code!

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
         "^.+\\.tsx?$": "ts-jest",
     },
     testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+    testPathIgnorePatterns: ["/lib/.*$"],
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
     collectCoverage: true,
     mapCoverage: true,
